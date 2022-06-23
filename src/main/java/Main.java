8f5d274031a9df419db0bd081ebcd6fe86e51a8c
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-23 10:59:33 -0400

    System exit added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("How old are you?");
+        int age=scanner.nextInt();
+        String response="";
+        if(age==18){
+            response="Wow! You're 18! Go get your license or something.";
+        }
+        else if(age<18 && age>=10){
+            response="You're not a kid anymore, but don't stop having fun!";
+        }
+        else if(age>18){
+            response="You're "+ age+".All grown up now. Congrats!";
+        }
+        else if(age<10){
+            response="Aww :) You're so young!";
+        }
+        System.out.println(response);
+        System.exit(0);        
     }
 }
+
