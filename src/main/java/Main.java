3a2e80403d452907bb32211e8dce1936f4c63118
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-23 15:39:51 -0400

    Added exception

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.println("How old are you?");
-        int age=scanner.nextInt();
+        String inputString=scanner.nextLine();
+        int age=input(inputString);
         String response="";
         if(age==18){
             response="Wow! You're 18! Go get your license or something.";
@@ -15,11 +16,23 @@ public class Main {
         else if(age>18){
             response="You're "+ age+".All grown up now. Congrats!";
         }
-        else if(age<10){
+        else if(age<10 && age>0){
             response="Aww :) You're so young!";
+        }
+        else {
+            System.out.println("Thats not a valid response.");
         }
         System.out.println(response);
         System.exit(0);        
+    }
+
+    private static int input(String sampleString) {
+        try {
+            return Integer.parseInt(sampleString);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
     }
 }
 
